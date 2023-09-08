4d140062717fd191ff5fabb5062988e50d5271bf
jiwon615 <seg615@naver.com>
2023-09-08 20:14:20 +0900

    test: reduce test

--- src/test/java/com/stream/jiwon/FinalApiTest.java
+++ src/test/java/com/stream/jiwon/FinalApiTest.java
@@ -1,4 +1,33 @@
 package com.stream.jiwon;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Optional;
+
 public class FinalApiTest {
+
+    @DisplayName("reduce test")
+    @Test
+    void reduce_test() {
+        // given
+        List<Integer> list = List.of(1, 2, 3, 4);
+
+        // when
+        Integer test1 = list.stream()
+                .reduce(Integer::sum)
+                .orElseThrow();
+
+        Integer test2 = list.stream()
+                .reduce(10, (a, b) -> a + b);
+
+        Integer test3 = list.stream()
+                .reduce(10, (a, b) -> a + b, (a, b) -> a + b);
+        //then
+        System.out.println("test1 = " + test1);
+        System.out.println("test2 = " + test2);
+        System.out.println("test3 = " + test3);
+
+    }
 }
