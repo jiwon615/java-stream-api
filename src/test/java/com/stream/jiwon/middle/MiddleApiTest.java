1422a1d9f4ad181124f78de3179c1cc001d24b28
jiwon615 <seg615@naver.com>
2023-09-20 20:14:20 +0900

    test: map test

--- src/test/java/com/stream/jiwon/middle/MiddleApiTest.java
+++ src/test/java/com/stream/jiwon/middle/MiddleApiTest.java
@@ -1,5 +1,6 @@
 package com.stream.jiwon.middle;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -7,6 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MiddleApiTest {
 
@@ -28,5 +30,21 @@ public class MiddleApiTest {
         //then
         assertThat(result).hasSize(2);
         assertThat(result2).hasSize(4);
+    }
+
+    @DisplayName("map test")
+    @Test
+    void map_test() {
+        // given
+        List<String> list = List.of("a", "a", "b", "b", "b", "c", "d");
+
+        // when
+        List<String> result = list.stream()
+                .map(String::toUpperCase)
+                .peek(System.out::print)
+                .toList();
+
+        //then
+        assertEquals(result.toString(), List.of("A, A, B, B, B, C, D").toString());
     }
 }
