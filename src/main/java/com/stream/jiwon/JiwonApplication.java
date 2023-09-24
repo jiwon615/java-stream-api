af9045a16f4c694ea8549cb012df999f17914f75
jiwon615 <seg615@naver.com>
2023-09-24 14:11:28 +0900

    chore: first setting

---
+++ src/main/java/com/stream/jiwon/JiwonApplication.java
@@ -0,0 +1,13 @@
+package com.stream.jiwon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiwonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiwonApplication.class, args);
+	}
+
+}
